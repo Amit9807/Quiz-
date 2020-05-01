e2deab649095847c460d3d8b3edfdc02e512c804
Amit9807 <samit9076@gmail.com>
2020-05-01 20:03:55 +0530

    v6

---
+++ myapplication/src/quizService/index.js
@@ -0,0 +1,368 @@
+const qBank = [
+  {
+    question:
+      "How many Times Amit Won against Vikas in TDM  ",
+    answers: ["2/4", "3/4", "1/4", "4/4"],
+    correct: "3/4",
+    questionId: "099099"
+  },
+  {
+    question:
+      'How many girlfriends sumit have till now ?',
+    answers: ["9", "12", "1", "Can't say"],
+    correct: "Can't say",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Which Indian Fast bowler have same action as pravin sahani ?",
+    answers: ["Umesh", "Bumrah", "Bhuvi", "Nehra"],
+    correct: "Bhuvi",
+    questionId: "267908"
+  },
+  {
+    question: "Who is that guy who make scratch on Manish face?",
+    answers: [
+      "Tengul",
+      "Padi",
+      "Sahid",
+      "Amit Kharat"
+    ],
+    correct: "Tengul",
+    questionId: "333247"
+  },
+  {
+    question: "Who is the Classical batsman in our team?",
+    answers: ["Sujit", "Amit", "Vikas", "Tengul"],
+    correct: "Tengul",
+    questionId: "496293"
+  },
+  {
+    question:
+      "Where is hitesh stay?",
+    answers: [
+      "Vashi",
+      "Khargar",
+      "Kamraj",
+      "Nerul"
+    ],
+    correct: "Nerul",
+    questionId: "588909"
+  },
+  {
+    question:
+      'Who is the Biggest Fekad in Our Team?',
+    answers: ["Tejas", "Tengul", "Vikas", "Sujit"],
+    correct: "Tejas",
+    questionId: "648452"
+  },
+  {
+    question: "Who is the Second Gachki in Our team?",
+    answers: ["Sujit", "Tengul", "Tejas", "Sumit"],
+    correct: "Tengul",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Who is the Best All Rounder in our Team?",
+    answers: ["Hitesh", "Tengul", "Sujit", "Vikas"],
+    correct: "Sujit",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Who is the Biggest Alsi in Out Team?",
+    answers: [
+      "Tengul",
+      "Vikas",
+      "Amit",
+      "Sujit"
+    ],
+    correct: "Tengul",
+    questionId: "98390"
+  },
+  {
+    question: "Who is the founder of Amazon?",
+    answers: ["MacKenzie Bezos", "Bill Gates", "jeff Bezos", "Zeny Bezos"],
+    correct: "jeff Bezos",
+    questionId: "1071006"
+  },
+  {
+    question: "Who is the Fastest Ball Bowled By an India Player ?",
+    answers: ["Javagal Srinath", "Varun Aaron", "Umesh", "Ishant Sharma"],
+    correct: "Javagal Srinath",
+    questionId: "1174154"
+  },
+  {
+    question: "Who is the man of Tournament in IPL 2019?",
+    answers: ["Bumrah", "Russel", "Warner", "Virat"],
+    correct: "Warner",
+    questionId: "1226535"
+  },
+  {
+    question: "Who Hits the Longest Six in the world?",
+    answers: ["AB Develiers", "Afridi", "Ms Dhoni", "Gayle"],
+    correct: "Adiós",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Skin", "Heart", "large Intestine", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "Second World War Happen in ?",
+    answers: ["1945", "1939", "1954", "1987"],
+    correct: "1939",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Which country Won most FIFA World Cup?",
+    answers: ["Argentina", "USA", "Brazil", "Portugal"],
+    correct: "Brazil",
+    questionId: "1822532"
+  },
+  {
+    question: "Which American president appears on a one dollar bill?",
+    answers: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Benjamin Franklin"
+    ],
+    correct: "George Washington",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "The book of Parsis is?",
+    answers: ["Torah", "Bible", "Zend Avesta", "Gita"],
+    correct: "Zend Avesta",
+    questionId: "2134343"
+  },
+  {
+    question: "Which one of the following rhythm games was made by Harmonix?",
+    answers: [
+      "Rock Band",
+      "Meat Beat Mania",
+      "Guitar Hero Live",
+      "Dance Dance Revolution"
+    ],
+    correct: "Rock Band",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "The National Anthem was first sung in the year?",
+    answers: ["1935", "1936", "1911", "1913"],
+    correct: "1911",
+    questionId: "2379831"
+  },
+  {
+    question: "Kathak is a classical dance of?",
+    answers: ["North India", "Tamilnadu", "	Manipur", "Kerala"],
+    correct: "North India",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the nickname of the US state of California?",
+    answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: "For how many days is a Test match scheduled?",
+    answers: [
+      "4",
+      "5",
+      "6",
+      "100 overs"
+    ],
+    correct: "5",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Who has taken the joint-most World Cup wickets for India?",
+    answers: ["Anil Kumble", "Zaheer Khan", "Kapil Dev", "Nehra"],
+    correct: "Zaheer Khan",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Pink", "Yellow", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: 'How far did India go in the 2015 World Cup?',
+    answers: ["Not Qualify", "Final", " Quater", " Semi"],
+    correct: "Semi",
+    questionId: "298060"
+  },
+  {
+    question: "Five dollars is worth how many nickles?",
+    answers: ["100", "50", "25", "69"],
+    correct: "100",
+    questionId: "3096579"
+  },
+  {
+    question: "Who is depicted on the US hundred dollar bill?",
+    answers: [
+      "Benjamin Franklin",
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "Benjamin Franklin",
+    questionId: "3182461"
+  },
+  {
+    question: "Where did India play its 1st one day international match?",
+    answers: [
+      "Lords",
+      "Headingley",
+      "Eden Garden",
+      "The Oval"
+    ],
+    correct: "Headingley",
+    questionId: "3239112"
+  },
+  {
+    question: "Who was the 1st ODI captain for India?",
+    answers: ["Ajit Wadekar", "Bishen Singh", "Kapil", "Gangully"],
+    correct: "Ajit Wadekar",
+    questionId: "3318503"
+  },
+  {
+    question: "What is the name of Poland in Polish?",
+    answers: ["Polska", "Pupcia", "Polszka", "Póland"],
+    correct: "Polska",
+    questionId: "3542596"
+  },
+  {
+    question: "Where did the 1st ODI match was played in India?",
+    answers: ["Mumbai", "Ahmedabad", "New Delhi", "Kolkata"],
+    correct: "Ahmedabad",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which restaurant's mascot is a clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Green", "Red", "Light Blue", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Who is First Prime minister",
+    answers: ["Indra Gandhi", "Nehru", "man-mohan", "Atal Bihari"],
+    correct: "Nehru",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What machine element is located in the center of fidget spinners?",
+    answers: ["Bearings", "Axles", "Gears", "Belts"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    answers: ["Libra", "Gemini", "Taurus", "Capricorn"],
+    correct: "Libra",
+    questionId: "4321002"
+  },
+  {
+    question: "Top Company in India?",
+    answers: [
+      "TATA",
+      "Reliance",
+      "Indian Oil",
+      "State Bank"
+    ],
+    correct: "Reliance",
+    questionId: "4442286"
+  },
+  {
+    question: "In School Kiska sabse bada Kata?",
+    answers: [
+      "Anurag",
+      "Sumit",
+      "Vikram",
+      "Vikas"
+    ],
+    correct: "Sumit",
+    questionId: "4564430"
+  },
+  {
+    question: "What company developed the vocaloid Hatsune Miku?",
+    answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
+    correct: "Crypton Future Media",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "What Indian city is the capital of two states?",
+    answers: ["Chennai", "Kolkata", "Chandigarh", "Mumbai"],
+    correct: "Chandigarh",
+    questionId: "4795960"
+  },
+  {
+    question: "What is the capital of Gujarat?",
+    answers: ["Surat", "Bhopal", "Gandhinagar", "Peshawar"],
+    correct: "Gandhi",
+    questionId: "4811162"
+  },
+  {
+    question: "How many nations border India?",
+    answers: ["5", "6", "7", "8"],
+    correct: "6",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
