af20e1b0ac6bce5462b8ccef1d56507e36021a04
Amit9807 <samit9076@gmail.com>
2020-05-01 21:14:17 +0530

    v9

---
+++ build/precache-manifest.bf2299735e7235332e64ccc6604dee4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbabf4d5ba0c25124d0ddf239796003e",
+    "url": "/Quiz-/index.html"
+  },
+  {
+    "revision": "b63d9dd6a39ed5653f24",
+    "url": "/Quiz-/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "db8d44628e6246177cfd",
+    "url": "/Quiz-/static/js/2.6438d3a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-/static/js/2.6438d3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63d9dd6a39ed5653f24",
+    "url": "/Quiz-/static/js/main.33bd91f5.chunk.js"
+  },
+  {
+    "revision": "d61253a61717853d087e",
+    "url": "/Quiz-/static/js/runtime-main.e5c9ebca.js"
+  }
+]);
